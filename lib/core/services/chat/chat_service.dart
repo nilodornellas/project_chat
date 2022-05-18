afa3ecc0d34f4262ba93f5fa480a8c70f3c1579d
nilodornellas <iannilo01@gmail.com>
2022-05-17 22:53:25 -0300

    Criando o chat service.

---
+++ lib/core/services/chat/chat_service.dart
@@ -0,0 +1,14 @@
+import 'package:projeto_chat/core/services/chat/chat_mock_service.dart';
+
+import '../../models/chat_message.dart';
+import '../../models/chat_user.dart';
+
+abstract class ChatService {
+  Stream<List<ChatMessage>> messagesStream();
+
+  Future<ChatMessage> save(String text, ChatUser user);
+
+  factory ChatService() {
+    return ChatMockService();
+  }
+}
